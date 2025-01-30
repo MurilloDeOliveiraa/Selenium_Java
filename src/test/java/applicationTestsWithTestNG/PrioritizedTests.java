42d29262f9b2aead586bd7b7d85a2c93fbc688e7
MurilloDeOliveiraa <murillo.lopes.oliveiraa@gmail.com>
2025-01-30 11:01:34 -0300

    added new testNg tags to test classes

--- src/test/java/applicationTestsWithTestNG/PrioritizedTests.java
+++ src/test/java/applicationTestsWithTestNG/PrioritizedTests.java
@@ -1,16 +1,27 @@
 package applicationTestsWithTestNG;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class PrioritizedTests {
 	
+	@BeforeTest
+	public void BeforeEachTest() {
+		System.out.println("--------Cleaned up all data--------");
+	}
+	
 	@Test
-	public void testOne() {
-		System.out.println("OK");
+	public void WebTest() {
+		System.out.println("WEB OK");
 	}
 
 	@Test
-	public void testTwo() {
-		System.out.println("OK");
+	public void AppleMobileTest1() {
+		System.out.println("MOBILE OK");
+	}
+	
+	@Test
+	public void AndroidMobileTest2() {
+		System.out.println("MOBILE OK");
 	}
 }
--- src/test/java/applicationTestsWithTestNG/SampleTests.java
+++ src/test/java/applicationTestsWithTestNG/SampleTests.java
@@ -1,16 +1,28 @@
 package applicationTestsWithTestNG;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SampleTests {
 	
-	@Test(description = "Basic Test")
-	public void BasicTest() {
-		System.out.println("Test 1 Passed!");
+	@BeforeTest
+	public void BeforeAllTests() {
+		System.out.println("--------Cleaned up all data--------");
 	}
 	
-	@Test(priority = 2)
+	@AfterTest
+	public void AfterAllTests() {
+		System.out.println("--------Finished all Tests--------");
+	}
+	
+	@Test(description = "Basic Test")
+	public void BasicTest() {
+		System.out.println("Basic Test 1 Passed!");
+	}
+	
+	@Test()
 	public void BasicTest2() {
-		System.out.println("Test 2 Passed!");
+		System.out.println("Basic Test 2 Passed!");
 	}
 }
